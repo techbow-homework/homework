5f0c15bf0b819212dac7d70d1359e96796daa8c1
Yike Gu <yikegu0215@Yikes-MBP.attlocal.net>
2021-09-26 17:09:10 -0700

    ssss

---
+++ src/main/java/com/techbow/homework/y2021/m09/yikegu/LC75.java
@@ -0,0 +1,40 @@
+package com.techbow.homework.y2021.m09.yikegu;
+
+import java.util.Arrays;
+
+public class LC75 {
+    public static void sortColors(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return;
+        }
+        int n = nums.length;
+        int curr = 0;
+        int zero = 0;
+        int two = n - 1;
+        while(curr < two) {
+            if(nums[curr] == 0) {
+                swap(nums, curr, zero);
+                zero++;
+                curr++;
+            }else if (nums[curr] == 2) {
+                swap(nums, curr, two);
+                two--;
+            }else{
+                curr++;
+            }
+        }
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[j];
+        nums[j] = nums[i];
+        nums[i] = temp;
+    }
+
+    public static void main(String[] arg) {
+        int[] test = new int[]{2,0,2,1,1,0};
+        sortColors(test);
+        System.out.println(Arrays.toString(test));
+
+    }
+}
