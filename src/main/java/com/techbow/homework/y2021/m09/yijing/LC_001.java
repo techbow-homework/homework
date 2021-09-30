4af57f50d88ffc7803d5c8256c4eaa96574acf59
YijingYang <839770193@qq.com>
2021-09-29 20:07:19 -0700

    update

--- src/main/java/com/techbow/homework/y2021/m09/yijing/LC_001.java
+++
@@ -1,4 +0,0 @@
-package com.techbow.homework.y2021.m09.yijing;
-
-public class LC_001 {
-}
---
+++ src/main/java/com/techbow/homework/y2021/m09/yijing/progress.java
@@ -0,0 +1,23 @@
+package com.techbow.homework.y2021.m09.yijing;
+
+import java.util.Stack;
+
+public class progress {
+    public static void main(String[] args) {
+        System.out.println("我可以！我能行！ 我爱学习！坚持就是胜利！");
+    }
+}
+
+// 09-26-2021
+// 算法陪练24录播
+
+// 09-27-2021
+// JavaOOD3录播
+
+// 09-28-2021
+// 算法陪练25直播
+
+// 09-29-2021
+// JAVAOOD4录播
+// JAVAOOD5录播
+
