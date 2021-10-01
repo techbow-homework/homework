61e590216b9f1bce6de75181a029bcc8ae77112b
aallen777 <lordhorcrux@gmail.com>
2021-09-30 22:44:37 -0700

    edited code in 215 and 283

--- src/main/java/com/techbow/homework/y2021/m09/mengyu/LC_215KthLargestInArray.java
+++ src/main/java/com/techbow/homework/y2021/m09/mengyu/LC_215KthLargestInArray.java
@@ -1,2 +1,10 @@
-package com.techbow.homework.y2021.m09.mengyu;public class LC_215KthLargestInArray {
+package com.techbow.homework.y2021.m09.mengyu;
+
+import java.util.Arrays;
+
+public class LC_215KthLargestInArray {
+    public int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+        return nums[nums.length - k];
+    }
 }
