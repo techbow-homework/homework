2a94a0d1f5c8efdaf74ce70569e302ad98939b3b
Xueyang Song <60683632+Xueyang-Song@users.noreply.github.com>
2021-10-04 15:55:52 -0700

    BFS_78

--- src/main/java/com/techbow/homework/y2021/m09/boyuan/小宋小宋/BFS_102.java
+++ src/main/java/com/techbow/homework/y2021/m09/boyuan/小宋小宋/BFS_102.java
@@ -4,6 +4,19 @@ import javax.swing.tree.TreeNode;
 import java.util.ArrayList;
 import java.util.List;
 
+
+public class TreeNode {
+    int val;
+     TreeNode left;
+    TreeNode right;
+     TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+          this.right = right;
+      }
+ }
 public class BFS_102 {
     List<List<Integer>> res = new ArrayList<>();
     public List<List<Integer>> levelOrder(TreeNode root) {
