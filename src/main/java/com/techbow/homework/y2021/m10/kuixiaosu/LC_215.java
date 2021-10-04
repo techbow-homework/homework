91b5ec93a5a761bf5c9645f7b888e4c04674a1a3
unknown <xiaoshz2@uci.edu>
2021-10-03 17:27:57 -0700

    new file

--- src/main/java/com/techbow/homework/y2021/m10/kuixiaosu/LC_215.java
+++ src/main/java/com/techbow/homework/y2021/m10/kuixiaosu/LC_215.java
@@ -1,2 +1,15 @@
-package com.techbow.homework.y2021.m10.kuixiaosu;public class LC_215 {
+package com.techbow.homework.y2021.m10.kuixiaosu;
+
+public class LC_215 {
+    public int findKthLargest(int[] nums, int k) {
+        if (nums == null || nums.length == 0) return -1;
+        PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>();
+        for(int i : nums) {
+            minHeap.add(i);
+            if (minHeap.size() > k){
+                minHeap.remove();
+            }
+        }
+        return minHeap.remove();
+    }
 }
