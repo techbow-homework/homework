11e95f34a3b284b1c9b12e0a29338dbf05222500
unknown <xiaoshz2@uci.edu>
2021-10-04 22:33:35 -0700

    new file

--- src/main/java/com/techbow/homework/y2021/m10/kuixiaosu/Card.java
+++ src/main/java/com/techbow/homework/y2021/m10/kuixiaosu/Card.java
@@ -1,2 +1,19 @@
-package com.techbow.homework.y2021.m10.kuixiaosu;public class Card {
+package com.techbow.homework.y2021.m10.kuixiaosu;
+
+public class Card {
+    private final int value;
+    private final Suit suit;
+
+    public Card(int value, Suit suit) {
+        this.value = value;
+        this.suit = suit;
+    }
+
+    public int getCardVal() {
+        return value;
+    }
+
+    public int getCardSuit() {
+        return Id;
+    }
 }
