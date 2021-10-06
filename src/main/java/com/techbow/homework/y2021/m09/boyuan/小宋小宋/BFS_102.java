be0d937a0f74579bf5954958ee5e184bb0f41547
abwwqqyy <qiyuewang0315@gmail.com>
2021-10-05 22:02:25 -0500

    10/05/2021QW bug fixing

--- src/main/java/com/techbow/homework/y2021/m09/boyuan/小宋小宋/BFS_102.java
+++ src/main/java/com/techbow/homework/y2021/m09/boyuan/小宋小宋/BFS_102.java
@@ -1,22 +1,11 @@
 package com.techbow.homework.y2021.m09.boyuan.小宋小宋;
 
-import javax.swing.tree.TreeNode;
+
 import java.util.ArrayList;
 import java.util.List;
 
 
-public class TreeNode {
-    int val;
-     TreeNode left;
-    TreeNode right;
-     TreeNode() {}
-    TreeNode(int val) { this.val = val; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
-         this.val = val;
-         this.left = left;
-          this.right = right;
-      }
- }
+
 public class BFS_102 {
     List<List<Integer>> res = new ArrayList<>();
     public List<List<Integer>> levelOrder(TreeNode root) {
