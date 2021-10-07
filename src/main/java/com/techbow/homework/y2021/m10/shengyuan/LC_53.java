2f1954b77777f8a6dc794976b03f885eeb22c8df
Shengyuan Cheng <scheng0718@gmail.com>
2021-10-06 21:16:27 -0700

    10/06/2021HW

--- src/main/java/com/techbow/homework/y2021/m10/shengyuan/LC_53.java
+++ src/main/java/com/techbow/homework/y2021/m10/shengyuan/LC_53.java
@@ -1,21 +1,27 @@
 package com.techbow.homework.y2021.m10.shengyuan;
 
 public class LC_53 {
-    public int lengthOfSubarray(int[] array) {
+    public int largestSum(int[] array) {
         if (array == null || array.length == 0) {
             return 0;
         }
-        int[] dp = new int[array.length];
-        dp[0] = 1;
-        int max = dp[0];
-        for (int i = 1; i < array.length; i++) {
-            if (array[i]> array[i - 1]) {
-                dp[i] = dp[i - 1] + 1;
+        int len = array.length;
+        int dp = array[0];
+        int max = dp;
+        for (int i = 1; i < len; i++) {
+            if (dp > 0) {
+                dp += array[i];
+                if (dp > max) {
+                    max = dp;
+                }
             } else {
-                dp[i] = 1;
+                dp = array[i];
             }
-            max = Math.max(dp[i], max);
         }
         return max;
+    }
+
+    public static void main(String[] args) {
+
     }
 }
