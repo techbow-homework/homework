a5b28782bd45c29aa906002ddaeb87810e386373
DanteWuuu <zw2521@columbia.edu>
2021-10-07 00:37:56 -0400

    LC

--- src/main/java/m10/dantewu/LC_01.java
+++ src/main/java/m10/dantewu/LC_01.java
@@ -4,17 +4,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class LC_01 {
-    class Solution {
-        public int[] twoSum(int[] nums, int target) {
-            Map<Integer, Integer> map = new HashMap<>();
-            for (int i = 0; i < nums.length; i ++){
-                int complement = target - nums[i];
-                if (map.containsKey(complement)) {
-                    return new int[] {map.get(complement), i};
-                }
-                map.put(nums[i], i);
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i ++){
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] {map.get(complement), i};
             }
-            return null;
+            map.put(nums[i], i);
         }
+        return null;
     }
 }
