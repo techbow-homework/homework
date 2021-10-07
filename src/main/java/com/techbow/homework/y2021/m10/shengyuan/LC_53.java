d7e930eb025cff18953169e0604e50ea80e733c8
scheng0718 <53288738+scheng0718@users.noreply.github.com>
2021-10-07 14:21:24 -0700

    deleted files

--- src/main/java/com/techbow/homework/y2021/m10/shengyuan/LC_53.java
+++ src/main/java/com/techbow/homework/y2021/m10/shengyuan/LC_53.java
@@ -1,31 +1,6 @@
 package com.techbow.homework.y2021.m10.shengyuan;
 
 public class LC_53 {
-<<<<<<< HEAD
-    public int largestSum(int[] array) {
-        if (array == null || array.length == 0) {
-            return 0;
-        }
-        int len = array.length;
-        int dp = array[0];
-        int max = dp;
-        for (int i = 1; i < len; i++) {
-            if (dp > 0) {
-                dp += array[i];
-                if (dp > max) {
-                    max = dp;
-                }
-            } else {
-                dp = array[i];
-            }
-        }
-        return max;
-    }
-
-    public static void main(String[] args) {
-
-    }
-=======
     public int lengthOfSubarray(int[] array) {
         if (array == null || array.length == 0) {
             return 0;
@@ -43,5 +18,4 @@ public class LC_53 {
         }
         return max;
     }
->>>>>>> e712b1360a2a1dfc15a458929f7f885292a30596
 }
