a9b60ef5bc0fa52e65f91e70d3d0fca0f68fd73a
Bill <Bill@Bills-MacBook-Pro.local>
2021-10-07 23:24:04 -0700

    LC_33

--- src/main/java/com/techbow/homework/y2021/m10/Runbiao/LC_33.java
+++ src/main/java/com/techbow/homework/y2021/m10/Runbiao/LC_33.java
@@ -1,4 +1,35 @@
 package com.techbow.homework.y2021.m10.Runbiao;
 
 public class LC_33 {
-}
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return -1;
+
+        int left = 0, right= nums.length-1;
+
+        while(left <= right){
+            int mid = left + (right-left)/2;
+
+            if(nums[mid]== target) return mid;
+            if(nums[left] == target) return left;
+            if(nums[right] == target) return right;
+
+           if(nums[left] < nums[mid]){
+               if(target > nums[left] && target < nums[mid]){
+                   right = mid - 1;
+               }
+               else{
+                   left = mid + 1;
+               }
+           }else{
+               if(target > nums[mid] && target < nums[right]) {
+                   left = mid + 1;
+               }else{
+                   right = mid - 1;
+               }
+               }
+           }
+            return -1;
+        }
+    }
+
+
