086b33a81b9199dca330a677d26661107d3dd375
LIN <zklin@bu.edu>
2021-10-11 02:25:07 -0400

    use stack to implement leftMinus and rightPlus.

--- src/main/java/com/techbow/homework/y2021/m10/zekun/LC0404_Tree_SumLeftLeaf.java
+++ src/main/java/com/techbow/homework/y2021/m10/zekun/LC0404_Tree_SumLeftLeaf.java
@@ -1,2 +1,19 @@
-package com.techbow.homework.y2021.m10.zekun;public class LC0404_Tree_SumLeftLeaf {
+package com.techbow.homework.y2021.m10.zekun;
+
+public class LC0404_Tree_SumLeftLeaf {
+    private int sum = 0;
+    public int sumOfLeftLeaves(TreeNode root) {
+        dfsHelper(root, false);
+        return sum;
+    }
+
+    private int dfsHelper(TreeNode root, boolean isLeft){
+        if(root == null) return 0;
+        if(root.left == null && root.right == null && isLeft){
+            sum += root.val;
+        }
+        if(root.left != null) dfsHelper(root.left, true);
+        if(root.right != null) dfsHelper(root.right, false);
+        return sum;
+    }
 }
