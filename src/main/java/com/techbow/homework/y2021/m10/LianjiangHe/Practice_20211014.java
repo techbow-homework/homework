e4bc2d631a54de6aa71d3c402762b702d71aadd3
NowDont <NowDont@users.noreply.github.com>
2021-10-14 23:46:52 -0700

    何莲江20210905 10/13/2021 (#249)
    * LianjiangHe20210905 2021/10/11
    
    * LianjiangHe20210905 2021/10/12
    
    * LianjiangHe20210905 2021/10/13
    
    * LianjiangHe20210905 2021/10/14

---
+++ src/main/java/com/techbow/homework/y2021/m10/LianjiangHe/Practice_20211014.java
@@ -0,0 +1,44 @@
+package com.techbow.homework.y2021.m10.LianjiangHe;
+
+public class Practice_20211014 {
+    public static void main(String[] args) {
+       /* B b = new B();
+        b.method1();
+        b.method3();*/
+        A2 c = new C();
+        c.method3();
+        c.method4();
+        ((C)c).method1();
+        ((A1)c).method1();
+    }
+}
+interface A1{
+    void method1();
+    void method2();
+}
+interface A2{
+    void method3();
+    void method4();
+}
+class C implements A1,A2{
+    @Override
+    public void method1() {
+        System.out.println("this is method 1");
+    }
+
+    @Override
+    public void method2() {
+        System.out.println("this is method 2");
+    }
+
+    @Override
+    public void method3() {
+        System.out.println("this is method 3");
+    }
+
+    @Override
+    public void method4() {
+        System.out.println("this is method 4");
+    }
+}
+
