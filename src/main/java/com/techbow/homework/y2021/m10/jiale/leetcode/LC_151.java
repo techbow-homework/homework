f8cdf4b9f080c1bf22528a2e60206e26d2d8d1e5
jiale <jd4678@nyu.edu>
2021-10-17 01:09:18 -0400

    LC_151

---
+++ src/main/java/com/techbow/homework/y2021/m10/jiale/leetcode/LC_151.java
@@ -0,0 +1,23 @@
+package com.techbow.homework.y2021.m10.jiale.leetcode;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Reverse Words in a String
+ */
+public class LC_151 {
+    public String reverseWords(String s) {
+        // remove leading spaces
+        s = s.trim();
+        // split by multiple spaces
+        List<String> wordList = Arrays.asList(s.split("\\s+"));
+        Collections.reverse(wordList);
+        return String.join(" ", wordList);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new LC_151().reverseWords("the sky is blue"));
+    }
+}
