6f609e7751e7b891c252097910f2ad7d66017b16
lianjiang He <dyk520@gmail.com>
2021-10-17 23:57:28 -0700

    LianjiangHe20210905 2021/10/17

--- src/main/java/com/techbow/homework/y2021/m10/LianjiangHe/Practice_20211016.java
+++ src/main/java/com/techbow/homework/y2021/m10/LianjiangHe/Practice_20211016.java
@@ -1,4 +1,11 @@
 package com.techbow.homework.y2021.m10.LianjiangHe;
 
 public class Practice_20211016 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("hello");
+        sb.append("world")
+                .append("!");
+        System.out.println(sb);
+    }
+
 }
