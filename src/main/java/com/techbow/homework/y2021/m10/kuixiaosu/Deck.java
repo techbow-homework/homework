ed6fa263d77a9a5159037294debbfd082124313b
unknown <xiaoshz2@uci.edu>
2021-10-18 22:31:42 -0700

    20210403 10/18/2021

--- src/main/java/com/techbow/homework/y2021/m10/kuixiaosu/Deck.java
+++ src/main/java/com/techbow/homework/y2021/m10/kuixiaosu/Deck.java
@@ -1,15 +1,17 @@
 package com.techbow.homework.y2021.m10.kuixiaosu;
 
 public class Deck {
+    //fields
     private ArrayList<Card> cards;
-    private Random random;
 
-    public Deck {
+    Singleton;
 
-        private List<Card> cards;
-        Random random;
-
-        public
-
+    public Deck() {
+        cards = new ArrayList<Card>();
+        for (Suit suit : Suit.values()) {
+            for (int i = 1; i <= 13; i++) {
+                cards.add(new Card(i, suit));
+            }
+        }
     }
 }
