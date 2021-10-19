64d243de8cc9e5d04a200cb1581d634c189b23ca
Yebo <fanqss1@gmail.com>
2021-10-18 22:49:36 -0700

    oct18

--- src/main/java/com/techbow/homework/y2021/m10/yeboyr/Oct17/Plant.java
+++ src/main/java/com/techbow/homework/y2021/m10/yeboyr/Oct17/Plant.java
@@ -1,2 +1,7 @@
-package com.techbow.homework.y2021.m10.yeboyr.Oct17;public class Plant {
+package com.techbow.homework.y2021.m10.yeboyr.Oct17;
+
+public class Plant {
+    public void grow(){
+        System.out.println("Plant growing");
+    }
 }
