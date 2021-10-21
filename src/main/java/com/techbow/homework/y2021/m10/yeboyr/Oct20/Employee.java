cafec6776407fd0dda9b1e48e8829a6c2703c8b7
Yebo <fanqss1@gmail.com>
2021-10-20 23:00:43 -0700

    yebo20210801 oct20

---
+++ src/main/java/com/techbow/homework/y2021/m10/yeboyr/Oct20/Employee.java
@@ -0,0 +1,11 @@
+package com.techbow.homework.y2021.m10.yeboyr.Oct20;
+
+public class Employee {
+    public static void main(String[] args) {
+        Person person1 = new Person();
+        person1.setInfo("Yueru","Lin",1,2000);
+        person1.raise(100);
+        System.out.println(person1);
+
+    }
+}
