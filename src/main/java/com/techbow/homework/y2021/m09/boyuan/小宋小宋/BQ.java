134ab91a4a0c591bd43074fc8737e10fffd0e136
Xueyang Song <60683632+Xueyang-Song@users.noreply.github.com>
2021-10-21 13:14:44 -0700

    lc

---
+++ src/main/java/com/techbow/homework/y2021/m09/boyuan/小宋小宋/BQ.java
@@ -0,0 +1,10 @@
+package com.techbow.homework.y2021.m09.boyuan.小宋小宋;
+
+public class BQ {
+//    Ø  Battery Test Results Collection & Search System                                                            05/2020 – 08/2020
+//    Implemented RESTful backend system lets the internal teams search battery test results using Spring Boot, Spring Data, MongoDB, and Elasticsearch.
+//    Refactored API request parameters with page number and page size.
+//    Analyzing battery test results with tokenization, token filters, and sorted by TF/IDF relevance.
+//    Used Kibana to create visualized reports for internal teams to analyze rate performance, voltage profile, and life-span of batteries with different electrolytes.
+
+}
