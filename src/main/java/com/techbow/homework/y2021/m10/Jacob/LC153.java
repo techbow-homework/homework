7538c02b5f4dfd2de56ea8bc4295162b60ce0a6b
jacobhuang <chokwu1993@gmail.com>
2021-10-22 15:25:59 -0700

    Oct 22

---
+++ src/main/java/com/techbow/homework/y2021/m10/Jacob/LC153.java
@@ -0,0 +1,27 @@
+package com.techbow.homework.y2021.m10.Jacob;
+
+public class LC153 {
+    public int findMin(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+
+        int left = 0;
+        int right = nums.length -1;
+        while (left + 1 < right) {
+            int mid = left + (right - left) /2;
+            if(nums[mid] > nums[nums.length -1]) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+        if(nums[left] < nums[right]) { //compare left and right find minimun.
+            return nums[left];
+        } else {
+            return nums[right];
+        }
+        //return nums[left];
+    }
+}
+
