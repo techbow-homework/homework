4e77519a44bca94eb3700bb4e8b7ac92ccc865aa
zanzantao <rantao0129@gmail.com>
2021-10-23 22:37:25 -0500

    reverse string 10/23/2021

---
+++ src/main/java/com/techbow/homework/y2021/m10/rantao/stringOct23.java
@@ -0,0 +1,12 @@
+package com.techbow.homework.y2021.m10.rantao;
+
+public class stringOct23 {
+    public void reverseString(char[] s) {
+        int left = 0, right = s.length - 1;
+        while (left < right) {
+            char tmp = s[left];
+            s[left++] = s[right];
+            s[right--] = tmp;
+        }
+    }
+}
