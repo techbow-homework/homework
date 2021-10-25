e52d7b16da9364439fd50268e1346b570a639c8b
Xueyang Song <60683632+Xueyang-Song@users.noreply.github.com>
2021-10-25 16:15:11 -0700

    bq

---
+++ src/main/java/com/techbow/homework/y2021/m09/boyuan/小宋小宋/BQ_differentIdea.java
@@ -0,0 +1,9 @@
+package com.techbow.homework.y2021.m09.boyuan.小宋小宋;
+
+public class BQ_differentIdea {
+//    S: First, I don’t have any personal conflict with my teammates  but once a time I have a different idea with my teammate.When I am working on my school project, we need to develop a supermarket membership notification backend system for a startup.
+//    T: My teammate thought we needed to use MySQL databases because we are more familiar with SQL, and it's good for fast analytical queries. But I thought we could use Nosql databases, such as MongoDB.
+//    A: First, I listed all the cons of noSQL. For example, it can handle large volumes of data at high speed with a scale-out architecture. After that, both of us talked with our prof for the third party thought, and he told us he also agreed that it’s better for us to use noSQL, because the project we are working for is focused on startups, these companies know the least about its business at the beginning, nosql databases enable easy updates to schemas and fields.
+//    R: When we decide which framework we can use for our project, the best idea is to analyze the customer’s needs, and find the most suitable one in this situation.
+
+}
