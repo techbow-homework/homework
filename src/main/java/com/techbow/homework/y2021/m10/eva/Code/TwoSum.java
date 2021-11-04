f4d1c7f39e0c0e95918cf347b53ee4af3fe3d58a
eva-yanyan <yanyanchen18@gmail.com>
2021-11-04 15:57:50 -0700

    LC_1twoSum

---
+++ src/main/java/com/techbow/homework/y2021/m10/eva/Code/TwoSum.java
@@ -0,0 +1,18 @@
+package com.techbow.homework.y2021.m10.eva.Code;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target){
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++){
+            int x = target - nums[i];
+            if(map.containsKey(x)){
+                return new int[] {map.get(x), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
