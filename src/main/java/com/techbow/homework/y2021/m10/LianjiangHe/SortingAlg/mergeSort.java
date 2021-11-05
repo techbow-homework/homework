a5a5b5f5d2caf1a82bcb79250ab85df96e537bc9
NowDont <NowDont@users.noreply.github.com>
2021-11-05 00:02:29 -0700

    LianjiangHe20210905 2021/11/05

---
+++ src/main/java/com/techbow/homework/y2021/m10/LianjiangHe/SortingAlg/mergeSort.java
@@ -0,0 +1,24 @@
+//arrayLIst version
+public  List<Integer> mergeSort(ArrayList<Integer> array){
+    if(array == null || array.size() == 0){
+        return null;
+        }
+    ArrayList<Integer> sortedArray = mergeSort(array, 0, array.size() - 1);
+        return sortedArray;
+        }
+
+private ArrayList<Integer> mergeSort(ArrayList<> array, int left, int right){
+    List<Integer> res = new ArrayList<Integer>();
+    if(left == right){
+        res.add(array.get(left));
+        return res;
+        }
+    int mid = left + (right - left)/2;
+    ArrayList<Integer> leftRes = mergeSort(array, left, mid);
+    ArrayList<Integet> rightRes = mergeSort(array, left, mid);
+    return merge(leftRes, rightRes);
+        }
+
+private ArrayList<Integer> merge(ArrayList<Integer> left,ArrayList<Integer> right){
+
+        }
