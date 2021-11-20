8cddfb792b3c3a2d52b19223c8d6057287f94f9a
merlinymy <lordhorcrux@gmail.com>
2021-11-20 00:36:34 -0800

    nov 19 LC796 186 mengyu20201207

--- src/main/java/com/techbow/homework/y2021/m11/mengyu/RemoveLeadingTrailingSpace.java
+++ src/main/java/com/techbow/homework/y2021/m11/mengyu/RemoveLeadingTrailingSpace.java
@@ -1,8 +1,30 @@
 package com.techbow.homework.y2021.m11.mengyu;
 
 public class RemoveLeadingTrailingSpace {
+    //S1:    keep the space after the word
+    // you_get_offer_ffer___
+    //               s
+    //                     f
     public String removeSpaces(String s) {
+        char[] sChar = s.toCharArray();
         int slow = 0;
+        int fast;
+        for (fast = 0; fast < s.length(); fast++) {
+            if (sChar[fast] == ' ' && (fast == 0 || sChar[fast - 1] == ' ')) {
+                continue;
+            } else {
+                sChar[slow++] = sChar[fast];
+            }
+        }
+        if (sChar[fast - 1] == ' ') {
+            return new String(sChar, 0, slow - 1);
+        }
+        return new String(sChar, 0, slow);
+    }
 
+    public static void main(String[] args) {
+        RemoveLeadingTrailingSpace test = new RemoveLeadingTrailingSpace();
+        String testString = "   I  will    get   offer";
+        System.out.println(test.removeSpaces(testString));
     }
 }
