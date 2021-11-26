c2611a6c272e1bb563b48127bcda4feca608503a
NowDont <NowDont@users.noreply.github.com>
2021-11-26 00:45:49 -0800

    LianjiangHe20210905 2021/11/25

--- src/main/java/com/techbow/homework/y2021/m10/LianjiangHe/SortingAlg/LC148.java
+++ src/main/java/com/techbow/homework/y2021/m10/LianjiangHe/SortingAlg/LC148.java
@@ -1,7 +1,7 @@
 //redo
 //insertion sort works but time limit exceeded
 //turns into merge sort
-//
+//time complexity O(nlogn)
 class Solution {
     public ListNode sortList(ListNode head) {
         if (head == null || head.next == null)
