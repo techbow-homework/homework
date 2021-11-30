1883ed3627004501cb67bbd74da06b94afb974b1
NowDont <NowDont@users.noreply.github.com>
2021-11-29 23:19:07 -0800

    LianjiangHe20210905 2021/11/29

--- src/main/java/com/techbow/homework/y2021/m10/LianjiangHe/SortingAlg/LC283.java
+++ src/main/java/com/techbow/homework/y2021/m10/LianjiangHe/SortingAlg/LC283.java
@@ -1,17 +1,15 @@
-/*
-void moveZeroes(vector<int>& nums) {
-        int lastNonZeroFoundAt = 0;
-        // If the current element is not 0, then we need to
-        // append it just in front of last non 0 element we found.
-        for (int i = 0; i < nums.size(); i++) {
-        if (nums[i] != 0) {
-        nums[lastNonZeroFoundAt++] = nums[i];
+//This is a 2 pointer approach
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int pos = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != 0){
+                nums[pos] = nums[i];
+                pos++;
+            }
         }
+        for(;pos<nums.length; pos++){
+            nums[pos] = 0;
         }
-        // After we have finished processing new elements,
-        // all the non-zero elements are already at beginning of array.
-        // We just need to fill remaining array with 0's.
-        for (int i = lastNonZeroFoundAt; i < nums.size(); i++) {
-        nums[i] = 0;
-        }
-        }*/
+    }
+}
